71d16015cacb485b52c7fdc08e3feabf6395ff73
pammimeow <pammimeow@gmail.com>
2015-10-21 14:08:31 +1100

    m

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicketApp::Application.config.secret_token = '4f7aced6b69c514bdcb690b257587ebdb72a51dbe0891e8e3d3cee0ba641595ce1f8a5ce2942eb44eca3fdb771479c2d69b150ee7fe89d2f365d478e3655dade'
